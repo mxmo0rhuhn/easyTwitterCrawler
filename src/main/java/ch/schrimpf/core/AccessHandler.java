208ce177353de5b8d62f7113ae7bcc21488b6fd2
Philipp Gamper <phgamper@student.ethz.ch>
2014-06-09 16:35:22 +0200

    clean up

--- src/main/java/ch/schrimpf/core/AccessHandler.java
+++ src/main/java/ch/schrimpf/core/AccessHandler.java
@@ -1,3 +1,23 @@
+/*
+* Copyright (c) 2014 Philipp Gamper and Max Schrimpf
+*
+* This file is part of the easy Twitter crawler project.
+* It enables you to specify a custom query and execute it for a while.
+*
+* It is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* The program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with the easy Twitter Crawler.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package ch.schrimpf.core;
 
 import twitter4j.Twitter;
