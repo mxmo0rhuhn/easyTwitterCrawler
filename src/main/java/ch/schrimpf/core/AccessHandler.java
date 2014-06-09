c85c32162e5a14dc40c01608c1eaff961549a4eb
Philipp Gamper <phgamper@student.ethz.ch>
2014-06-09 15:27:03 +0200

    AccessHandler created

---
+++ src/main/java/ch/schrimpf/core/AccessHandler.java
@@ -0,0 +1,77 @@
+package ch.schrimpf.core;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+import java.io.*;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AccessHandler {
+
+    private static final Logger LOG = Logger.getLogger(AccessHandler.class.getName());
+
+    public static AccessToken register(String key, String secret) throws TwitterException {
+        // The factory instance is re-useable and thread safe.
+        Twitter twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer(key, secret);
+        RequestToken requestToken = twitter.getOAuthRequestToken();
+        AccessToken accessToken = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (null == accessToken) {
+            System.out.println("Open the following URL and grant access to your account:");
+            System.out.println(requestToken.getAuthorizationURL());
+            System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+            try {
+                String pin = br.readLine();
+                if (pin.length() > 0) {
+                    accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                } else {
+                    accessToken = twitter.getOAuthAccessToken();
+                }
+            } catch (TwitterException te) {
+                if (401 == te.getStatusCode()) {
+                    System.out.println("Unable to get the access token.");
+                } else {
+                    te.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            FileOutputStream fout = new FileOutputStream("token.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            oos.writeObject(accessToken);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, "Could not store access token.");
+        }
+        return accessToken;
+    }
+
+    public static AccessToken loadToken() {
+        try {
+            FileInputStream fin = new FileInputStream("token.ser");
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            return (AccessToken) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            LOG.log(Level.WARNING, "could not dematerialize stored token");
+        }
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileInputStream("api.key"));
+            return register(prop.getProperty("apiKey"), prop.getProperty("apiSecret"));
+        } catch (IOException e) {
+            // Properties could not be load
+            LOG.log(Level.SEVERE, "could not get api key properties");
+        } catch (TwitterException e) {
+            LOG.log(Level.SEVERE, "could not get new token");
+        }
+        return null;
+    }
+}
--- src/main/java/ch/schrimpf/core/TwitterCrawler.java
+++ src/main/java/ch/schrimpf/core/TwitterCrawler.java
@@ -1,24 +1,24 @@
 package ch.schrimpf.core;
 
 import twitter4j.Query;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 
 public class TwitterCrawler implements Runnable {
 
-    private Twitter twitter;
     private AccessToken accessToken;
     private Query query;
 
-    public TwitterCrawler(String key, String secret, String query) throws TwitterException {
+    public TwitterCrawler(AccessToken token, String query){
 
         this.query = new Query(query);
+
+
+
+        /*
         twitter = new TwitterFactory().getInstance();
         twitter.setOAuthConsumer(key, secret);
         accessToken = twitter.getOAuthAccessToken();
-        new Thread(this).start();
+        new Thread(this).start();*/
     }
 
     @Override
@@ -32,5 +32,4 @@ public class TwitterCrawler implements Runnable {
             }
         }*/
     }
-
 }
